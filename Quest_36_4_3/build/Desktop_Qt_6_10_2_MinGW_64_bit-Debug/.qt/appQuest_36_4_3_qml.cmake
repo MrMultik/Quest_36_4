8efe36d4d4f003371c675647b57eeec31f6e261c
Multik <ilyakondaurov5@gmail.com>
2026-03-12 13:11:49 +0300

    Add build folder files

---
+++ Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/appQuest_36_4_3_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appQuest_36_4_3")
+set(working_dir "C:/Users/User/Desktop/C/Test/Quest_36_4_3")
+set(src_and_dest_list
+    "C:/Users/User/Desktop/C/Test/Quest_36_4_3/Main.qml"
+    "C:/Users/User/Desktop/C/Test/Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Quest_36_4_3/Main.qml"
+
+)
+set(timestamp_file "C:/Users/User/Desktop/C/Test/Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/appQuest_36_4_3_qml.txt")
---
+++ Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/appQuest_36_4_3_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appQuest_36_4_3")
+set(working_dir "C:/Users/User/Desktop/C/Test/Quest_36_4_3")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/Users/User/Desktop/C/Test/Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/appQuest_36_4_3_res.txt")
