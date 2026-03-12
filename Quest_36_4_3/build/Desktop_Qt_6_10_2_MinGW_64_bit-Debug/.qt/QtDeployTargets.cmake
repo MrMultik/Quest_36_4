40bfc6e485a2f787280c51522a6343c3f7f5be0d
Multik <ilyakondaurov5@gmail.com>
2026-03-12 13:14:43 +0300

    Re-add all project folders

--- Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Quest_36_4_FILE C:/Users/User/Desktop/C/Test/Quest_36_4_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Quest_36_4.exe)
-set(__QT_DEPLOY_TARGET_Quest_36_4_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Quest_36_4_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
